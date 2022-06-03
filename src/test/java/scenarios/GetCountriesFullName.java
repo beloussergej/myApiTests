d638981cc931c1b8d65161cc051ab6896c817050
Белоус Сергей <105642110+beloussergej@users.noreply.github.com>
2022-06-03 11:51:59 +0300

    Add Class

--- src/test/java/scenarios/GetCountriesFullName.java
+++ src/test/java/scenarios/GetCountriesFullName.java
@@ -1,8 +1,10 @@
 package scenarios;
 
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class GetCountriesFullName {
 
     private void getCountriesInvalid(){
-
+        RSAOtherPrimeInfo()
     }
 }
